a4b3850c4016f56ea632f5c3a4c6b0c8d28bdc53
Raleigh Dale <dalearaleigh@gmail.com>
2019-03-28 22:31:36 +0000

    who knows what i'm doing

--- db/migrate/20190327220742_create_photos.rb
+++ db/migrate/20190327220742_create_photos.rb
@@ -5,7 +5,8 @@ class CreatePhotos < ActiveRecord::Migration[5.2]
   def change
     create_table :photos do |t|
       t.text :caption
-      t. integer :place_id
+      t.integer :place_id
+      t.string :picture
       t.timestamps
     end
   end
