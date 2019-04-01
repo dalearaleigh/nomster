b7feebfdacdb21643f5fed6c477f0ae4ddca58bc
Raleigh Dale <dalearaleigh@gmail.com>
2019-04-01 16:04:17 +0000

    troubleshoot push

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -1,10 +1,1 @@
 # config/initializers/carrierwave.rb
-
-CarrierWave.configure do |config|
-  config.fog_provider = 'fog/aws'                        # required
-  config.fog_credentials = {
-    provider:              'AWS',                        # required
-    aws_access_key_id:     ENV["AIzaSyAJBoUl8RFkSF-1c5JDYRQ-haYpDRKn6a4"],        # required
-    aws_secret_access_key: ENV["xnVTjI758gM1ADowMlg+Qe+Kxi8NUBGqe6Ba/9Aq"],        # required
-  }
-  config.fog_directory  = ENV["nomster-rad-bucket"]              # required
